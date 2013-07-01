c22da84eeff53a45a247597a0b0044f48b4aef51
Kendall Park and Luke Booth <pair=kendall.park+luke.booth@cphepdev.com>
2013-07-01 15:02:13 -0500

    [skip] Changed the package opener

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 class keycastr {
 
   package { 'keycastr':
-    provider => 'appdmg',
+    provider => 'pkgdmg',
     source   => "http://www.brothersoft.com/d.php?soft_id=319359&url=http%3A%2F%2Ffiles.brothersoft.com%2Fmac%2Fbusiness%2Fpresentation%2Fkeycastr_0.8.0.dmg",
   }
 }
