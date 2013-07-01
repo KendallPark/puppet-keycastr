cc6f4c65307affb3d20769a19698517236bfa2a6
Kendall Park and Luke Booth <pair=kendall.park+luke.booth@cphepdev.com>
2013-07-01 15:24:06 -0500

    added ensure installed

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,6 @@ class keycastr {
   package { 'keycastr':
     provider => 'appdmg',
     source   => "http://www.brothersoft.com/d.php?soft_id=319359&url=http%3A%2F%2Ffiles.brothersoft.com%2Fmac%2Fbusiness%2Fpresentation%2Fkeycastr_0.8.0.dmg",
+    ensure   => installed
   }
 }
